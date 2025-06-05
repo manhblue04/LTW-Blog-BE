291dbeab2e1cbfed72723573a581833daa770831
Manh Blue <manhducnb204@gmail.com>
2025-06-05 11:31:16 +0700

    atlas

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function dbConnect() {
-  mongoose
-    .connect(process.env.DB_URL)
+  await mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log("Successfully connected to MongoDB Atlas!");
     })
